8826fbc0419fbe8e1e280c3459bc06bfe9cf9548
ctoverton <christianoverton@ctoverton.com>
2024-05-17 09:14:51 -0600

    updated latest post

--- src/config/latestPost.ts
+++ src/config/latestPost.ts
@@ -1,13 +1,3 @@
-export const postTitle = 'RSS Feed'
-export const postDate = 'May 14th'
-export const postContent = `Hey everyone, We're still sorting out some paperwork regarding
-                our regular RSS feed and YouTube channel so, for our first
-                episode, we're going to use our brand new Patreon for hosting.
-                You can use our Patreon RSS this week to listen to our first
-                episode at 10PM CT TONIGHT! That's right, because of the
-                technical hurdles we're putting everyone through, and because
-                we're kind of too excited to wait anyway, we're releasing our
-                first episode of Regulation Podcast tonight at 10pm CT. If you
-                prefer to get it on youtube, you can. We'll be posting it to our
-                Regulation Podcast Gaming Channel which is right here:
-                https://www.youtube.com/@TheRegulationPodcast`
+export const postTitle = "Sloppy Joe's Bingo 5/11/24 // Regulation VOD"
+export const postDate = 'May 17th'
+export const postContent = `It's Saturday night and the Regulation Podcast crew is here for more bingo! Join Andrew, Nick, Eric, Geoff, and Gavin (ANEGG) as they play Sloppy Joe's Bingo with you! Originally streamed 5/11/24. Tune in for Regulation livestreams every Friday on twitch.tv/theregulationpod.`
