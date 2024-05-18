469ade0afc62e355de9aa50fb11a1d4ad052832a
ctoverton <christianoverton@ctoverton.com>
2024-05-18 16:42:49 -0600

    updated latest post

--- src/config/latestPost.ts
+++ src/config/latestPost.ts
@@ -1,3 +1,3 @@
-export const postTitle = "Sloppy Joe's Bingo 5/11/24 // Regulation VOD"
-export const postDate = 'May 17th'
-export const postContent = `It's Saturday night and the Regulation Podcast crew is here for more bingo! Join Andrew, Nick, Eric, Geoff, and Gavin (ANEGG) as they play Sloppy Joe's Bingo with you! Originally streamed 5/11/24. Tune in for Regulation livestreams every Friday on twitch.tv/theregulationpod.`
+export const postTitle = 'Worms Part 1 // Regulation Gameplay'
+export const postDate = 'May 18th'
+export const postContent = `Gavin's out so it's time to find a new Worms champion! Who will be the wormiest worm? Join Andrew, Nick, Eric and Geoff as they find out!`
